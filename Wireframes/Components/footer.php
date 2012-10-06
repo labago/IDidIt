c85deacf5b1214039db74968aa03610651c58de7
jon <jlane09sjp@gmail.com>
2012-10-06 02:23:10 -0400

    basic site structure done

---
+++ Wireframes/Components/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+	<h1>Footer</h1>
+
+</div>
---
+++ Wireframes/Pages/master.php
@@ -0,0 +1,19 @@
+<html>
+	<title>IDidIt</title>
+	<head>
+		<link href="../../styles/style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<?php include("../Components/header.php"); ?>
+
+		<div class="page">
+			<div class="content">
+				<h1>Body</h1>
+
+
+			</div>
+		</div>
+
+		<?php include("../Components/footer.php"); ?>
+	</body>
+</html>
