09213af9773b44fa9434778c06d3f773a1af2757
jon <jlane09sjp@gmail.com>
2012-10-14 00:45:50 -0400

    added 404 error page

---
+++ missing.php
@@ -0,0 +1,24 @@
+<?php include("functions.php"); ?>
+<html>
+	<title>IDidIt</title>
+	<head>
+		<link href="styles/style.css" rel="stylesheet" type="text/css">
+	</head>
+	<script type="text/javascript" src="scripts/jquery.js"></script>
+	<script type="text/javascript" src="scripts/jquery.tokeninput.js"></script>
+	<link rel="stylesheet" type="text/css" href="styles/token-input.css" />
+	<script type="text/javascript" src="scripts/functions.js"></script>
+	<body>
+		<?php include("Components/header.php"); ?>
+
+		<div class="page">
+			<div class="content">
+				<h2>404 Error Page</h2>
+				<p>This page does not exist, not sure how you got here</p>
+
+			</div>
+		</div>
+
+		<?php include("Components/footer.php"); ?>
+	</body>
+</html>
