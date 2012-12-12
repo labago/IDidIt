fe3ef83f6c01d81b9c951df998230fe22a097ce4
Jonathan Lane <jlane09sjp@gmail.com>
2012-12-12 08:32:47 -0500

    changees

--- login_user.php
+++ login_user.php
@@ -6,5 +6,7 @@ $crypt = $_GET['c'];
 change_log_out_status_fb($crypt, "false");
 setcookie("user", $crypt, time()+2592000);
 
-echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
+echo $crypt;
+
+//echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 ?>
